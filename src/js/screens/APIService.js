ec2c283ab46084f853907af83a6c7f1b60be9696
Peter Munro <peter@petermunro.org>
2022-05-27 14:10:07 +0000

    Mocked up a form for APIService

--- src/js/screens/APIService.js
+++ src/js/screens/APIService.js
@@ -1,18 +1,77 @@
-import Paragraph from "grommet/components/Paragraph";
-import React from "react";
+import React, { Component } from "react";
 
-function APIService() {
-  return (
-    <Paragraph>
-      Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh
-      start in the New Zealand countryside. He quickly finds himself at home
-      with his new foster family: the loving Aunt Bella, the cantankerous Uncle
-      Hec, and dog Tupac. When a tragedy strikes that threatens to ship Ricky to
-      another home, both he and Hec go on the run in the bush. As a national
-      manhunt ensues, the newly branded outlaws must face their options: go out
-      in a blaze of glory or overcome their differences and survive as a family.
-    </Paragraph>
-  );
+import Box from "grommet/components/Box";
+import Button from "grommet/components/Button";
+import Footer from "grommet/components/Footer";
+import Form from "grommet/components/Form";
+import FormField from "grommet/components/FormField";
+import Header from "grommet/components/Header";
+import Heading from "grommet/components/Heading";
+import Label from "grommet/components/Label";
+import Select from "grommet/components/Select";
+import TextInput from "grommet/components/TextInput";
+
+class APIService extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entitlementId: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ entitlementId: e.target.value });
+  }
+
+  render() {
+    return (
+      <Box pad="medium">
+        <Form>
+          <Header>
+            <Heading strong={false}>API Service</Heading>
+          </Header>
+
+          <FormField>
+            <Label>Entitlement ID</Label>
+            <TextInput
+              id="entitlemendId"
+              name="entitlemendId"
+              value={this.state.entitlementId}
+              onDOMChange={this.handleChange}
+            />
+          </FormField>
+
+          <FormField>
+            <Label>Type Code</Label>
+            <Select
+              options={[
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+              ]}
+              value={undefined}
+              onChange={null}
+            />
+          </FormField>
+
+          <Footer pad={{ vertical: "medium" }}>
+            <Button
+              label="Submit"
+              type="submit"
+              primary={true}
+              onClick={null}
+            />
+          </Footer>
+        </Form>
+      </Box>
+    );
+  }
 }
 
 export default APIService;
