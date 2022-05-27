11c2238b4035ba6aa9e9384d4830371f85000cbe
Peter Munro <peter@petermunro.org>
2022-05-27 12:49:43 +0000

    Initial commit

---
+++ src/js/screens/APIService.js
@@ -0,0 +1,18 @@
+import Paragraph from "grommet/components/Paragraph";
+import React from "react";
+
+function APIService() {
+  return (
+    <Paragraph>
+      Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh
+      start in the New Zealand countryside. He quickly finds himself at home
+      with his new foster family: the loving Aunt Bella, the cantankerous Uncle
+      Hec, and dog Tupac. When a tragedy strikes that threatens to ship Ricky to
+      another home, both he and Hec go on the run in the bush. As a national
+      manhunt ensues, the newly branded outlaws must face their options: go out
+      in a blaze of glory or overcome their differences and survive as a family.
+    </Paragraph>
+  );
+}
+
+export default APIService;
